bebb868b478300937056c2fac8d7c21516ba300c
omprakashgupta82 <omprakash.gupta@outlook.com>
2019-01-28 22:30:26 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessageComponent } from './message/message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,32 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    { id: 11, name : 'Mr. Nice'},
+    { id: 12, name : "Narco"},
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+]
+
+// export class MockHeroes
+// {
+//      HEROES: Hero[];
+
+//      getMockHeroes(): Hero[]
+//      {
+//         var i:number;
+
+//          for(i=1;i<=10;i++)
+//          {
+//              console.log(i.toString());
+//             this.HEROES.push({id : i, name : "Om " + i.toString()}) 
+//          }
+
+//          return this.HEROES;
+//      }
+// }
